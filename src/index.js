aa6a83ae0b97b3f1a0a1d5e12e2273ef6ec81417
Furkan Karataş <furkan.karatas02@gmail.com>
2019-08-19 17:43:48 +0300

    Add firebase integration

--- src/index.js
+++ src/index.js
@@ -6,8 +6,18 @@ import * as serviceWorker from './serviceWorker'
 import { createStore, applyMiddleware, compose } from "redux"
 import { Provider } from "react-redux"
 import rootReducer from "./reducers/rootReducer"
+import thunk from "redux-thunk"
+import {reduxFirestore, getFirestore} from "redux-firestore"
+import {reactReduxFirebase, getFirebase} from "react-redux-firebase"
+import fbConfig from "./config/firebaseConfig"
 
-const store = createStore(rootReducer)
+const store = createStore(rootReducer,
+  compose(
+    applyMiddleware(thunk.withExtraArgument({getFirebase, getFirestore})),
+    reduxFirestore(fbConfig),
+    reactReduxFirebase(fbConfig)
+  )
+)
 
 ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
 
