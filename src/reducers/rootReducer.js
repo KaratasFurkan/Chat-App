e11243cebfe960ef8db4de92c05d142061438e09
Furkan Karataş <furkan.karatas02@gmail.com>
2019-08-19 17:52:40 +0300

    Add firebase integration

--- src/reducers/rootReducer.js
+++ src/reducers/rootReducer.js
@@ -1,8 +1,10 @@
 import messageReducer from "./messageReducer"
 import {combineReducers} from "redux"
+import {firestoreReducer} from "redux-firestore"
 
 const rootReducer = combineReducers({
   message: messageReducer,
+  firestore: firestoreReducer
 })
 
 export default rootReducer
