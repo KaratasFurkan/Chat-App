daeba875e96d3e120d1ebf1e076fb80bd0ce8baf
Furkan Karataş <furkan.karatas02@gmail.com>
2019-08-19 17:29:32 +0300

    Add routers

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
-import React from 'react';
+import React, { Component } from 'react';
+import ChatRoom from "./components/ChatRoom"
+import Welcome from "./components/Welcome"
+import { BrowserRouter, Route } from "react-router-dom"
 
-function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+class App extends Component{
+  render() {
+    return (
+      <BrowserRouter>
+        <div className="App">
+          <Route exact path="/" component={Welcome} />
+          <Route path="/chatroom" component={ChatRoom} />
+        </div>
+      </BrowserRouter>
+    )
+  }
 }
 
-export default App;
+export default App
