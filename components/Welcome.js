72464afcf8141f5fa5dd3e7348219f27ca1cecae
Furkan Karataş <furkan.karatas02@gmail.com>
2019-08-19 17:24:40 +0300

    Create Welcome component

---
+++ components/Welcome.js
@@ -0,0 +1,28 @@
+import React, { Component } from "react"
+
+class Welcome extends Component {
+  handleSubmit = (e) => {
+    e.preventDefault()
+    this.props.history.push({
+      pathname: "/chatroom",
+      state: {
+        name: [e.target.name.value]
+      }
+    })
+  }
+  
+  render() {
+    return (
+      <div className="Welcome">
+        <h1>Welcome</h1>
+        <h3>Enter your name:</h3>
+        <form onSubmit={this.handleSubmit}>
+          <input type="text" name="name"/>
+          <button id="sendButton">Enter</button>
+        </form>
+      </div>
+    )
+  }
+}
+
+export default Welcome
